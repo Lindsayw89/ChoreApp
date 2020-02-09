b811fd0f290af26a55bd3fbc1d768f9dc771dda6
lindsayw89 <lindsay.89@outlook.com>
2020-02-09 00:16:43 -0600

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {AuthGuard} from './auth.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path: 'userprofile', component: UserProfileComponent, canActivate: [AuthGuard]}];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
